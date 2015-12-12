73049ca28cc32f6efc860a0d9a422efa642caa35
Daniel Weiss <mail@weiss-daniel.de>
2015-12-12 13:06:24 +0100

    remove app folder. new repo => messyOne/Loo-App

--- app/cli-config.php
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env php
-<?php
-include_once('private/bootstrap.php');
-
-$factory = new \Loo\Database\DatabaseFactory();
-$entityManager = $factory->getEntityManager();
-
-return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($entityManager);
--- app/private/constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// TODO check which are needed. get rid of them
-define('ROOT', dirname(__DIR__));
-define('APPLICATION', 'App');
-define('FRAMEWORK', 'Loo');
-define('EXTERNALS', 'vendor');
-define('CONTROLLER', 'Controller');
